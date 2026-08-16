e0862dfcd3f0c01876f36f822a08d8a3f756b2d2
gaayathrikk <gaayathrikk2001@gmail.com>
2026-08-17 01:01:54 +0530

    Cucumber test config

---
+++ iss-predictor-service/src/test/java/com/isspredictor/iss_predictor_service/bdd/CucumberSpringConfiguration.java
@@ -0,0 +1,9 @@
+package com.isspredictor.iss_predictor_service.bdd;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+}
--- iss-predictor-service/src/test/java/com/isspredictor/iss_predictor_service/bdd/CucumberTest.java
+++ iss-predictor-service/src/test/java/com/isspredictor/iss_predictor_service/bdd/CucumberTest.java
@@ -6,14 +6,12 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-//  Entry point that makes surefire discover and run the Cucumber feature files.
-//  Named "CucumberTest" (not "CucumberSuite" or similar) specifically because
-//  our pom.xml's surefire config only includes **/*Test.java - this class
-//  wouldn't be picked up at all under a different naming pattern.
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.isspredictor.bdd")
+@ConfigurationParameter(
+        key = Constants.GLUE_PROPERTY_NAME,
+        value = "com.isspredictor.iss_predictor_service.bdd"
+)
 public class CucumberTest {
 }
